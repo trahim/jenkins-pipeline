81f5f34a5f817d448fc19966058db8aad0ff8a7a
Tasneem Rahim <trahim@zaizi.com>
2017-05-04 10:20:25 +0100

    separating functions

---
+++ jobs.groovy
@@ -0,0 +1,31 @@
+getJobs() {
+    def pipelineFile = readProperties file: "pipelines.txt"
+    pipelines = pipelineFile['pipelines']
+    pipelines.each {
+        createJob($it)
+    }
+}
+
+createJob(name){
+    step([
+            $class: "ExecuteDslScripts",
+            lookupStrategy: "SEED_JOB",
+            scriptText: """
+                pipelineJob("${name}") {
+                    definition {
+                        cpsScm {
+                            scm {
+                                git {
+                                    remote {
+                                        url("${gitUrl}")
+                                        branch("${gitBranch}")
+                                    }
+                                }
+                            }
+                            scriptPath("${name}")
+                        }
+                    }
+                }
+            """
+    ])
+}
